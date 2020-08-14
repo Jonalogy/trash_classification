29c3663ebd546ca5cd0b8c25d52b08cc6fc6778e
Arjun Taneja <ataneja@palo-it.com>
2020-08-14 17:59:28 +0800

    Adjusted capture script to make counter file hidden

--- local/scripts/capture.py
+++ local/scripts/capture.py
@@ -8,7 +8,7 @@ index = [ '', 'paper', 'plastic', 'metal', 'glass', 'other' ]
 
 #categories
 # 1 - Paper
-# 2 - Plastic 
+# 2 - Plastic
 # 3 - Metal
 # 4 - Glass
 # 5 - Other
@@ -16,15 +16,15 @@ index = [ '', 'paper', 'plastic', 'metal', 'glass', 'other' ]
 while True:
 	inp = int(input(''))
 	material = index[inp]
-	
+
 	counter = 0
-	with open(f'../images/{material}/counter.txt', 'r') as f:
+	with open(f'../images/{material}/.counter.txt', 'r') as f:
 		for line in f:
 			counter = int(line) + 1
 
-	with open(f'../images/{material}/counter.txt', 'w') as f:
+	with open(f'../images/{material}/.counter.txt', 'w') as f:
 		f.write(str(counter))
-	
+
 	path = f'../images/{material}/{material}{counter}.jpg'
 	camera.capture(path)
 
