4ba8eb0756944b575b20930d09feacf7be44581e
Jonathan Tan <jontan@palo-it.com>
2020-10-20 04:34:35 +0800

    Update methods to split data to test, validation and train set

--- helpers.py
+++ helpers.py
@@ -1,10 +1,17 @@
 import random
+from math import floor
 
 
-def split_train_test(image_paths, train_ratio):
+def split_train_test(image_paths, train_ratio, validation_ratio):
     random.shuffle(image_paths)
     total_image_count = len(image_paths)
-    train_count = round(total_image_count * train_ratio)
-    test_count = total_image_count - train_count
-    print(f'total: {len(image_paths)}, train: {train_count}, test:{test_count}')
-    return [image_paths[:train_count], image_paths[train_count:]]
+    train_count = floor(total_image_count * train_ratio)
+    validation_count = floor(total_image_count * validation_ratio)
+    test_count = total_image_count - train_count - validation_count
+    print(f'Total images found: {len(image_paths)}')
+    print(f'Train: {train_count}, Validation: {validation_count}, Test: {test_count}')
+    return [
+        image_paths[:train_count],
+        image_paths[train_count: (train_count+validation_count)],
+        image_paths[train_count+validation_count:]
+    ]
