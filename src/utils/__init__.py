e731b68ddfaef515f36cf3b3945906342471be0a
Jonathan Tan <jontan@palo-it.com>
2020-10-23 20:08:00 +0800

    Update utils.__ini__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,3 +2,4 @@ from .argmax_int import *
 from .extract_truth_and_predictions import *
 from .log_confusion_matrix import *
 from .plot_confusion_matrix import *
+from .log_wrongly_classified_images import *
