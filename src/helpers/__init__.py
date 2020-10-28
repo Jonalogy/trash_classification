59ab1500497f4fe5bc8f54f1e20acac219b340ba
Jonathan Tan <jontan@palo-it.com>
2020-10-28 14:43:48 +0800

    Implement helper functions to split datasets to test train and validation sets

---
+++ src/helpers/__init__.py
@@ -0,0 +1,1 @@
+from .split_train_valid_test import split_train_valid_test
---
+++ src/helpers/partition.py
@@ -0,0 +1,17 @@
+import random
+from math import floor
+
+
+def partition(image_paths, train_ratio, validation_ratio):
+    random.shuffle(image_paths)
+    total_image_count = len(image_paths)
+    train_count = floor(total_image_count * train_ratio)
+    validation_count = floor(total_image_count * validation_ratio)
+    test_count = total_image_count - train_count - validation_count
+    print(f'Total images found: {len(image_paths)}')
+    print(f'Train: {train_count}, Validation: {validation_count}, Test: {test_count}')
+    return [
+        image_paths[:train_count],
+        image_paths[train_count: (train_count+validation_count)],
+        image_paths[train_count+validation_count:]
+    ]
