64a5e19f51955c479d6f163f6bc6a542c8ed594f
Jonathan Tan <jontan@palo-it.com>
2020-10-29 16:01:10 +0800

    Update param name to differentiate that it takes the instance of the model builder instate of the model itself

--- src/pipelines/fine_tune_model.py
+++ src/pipelines/fine_tune_model.py
@@ -3,22 +3,22 @@ from tensorflow.keras.callbacks import TensorBoard
 import os
 
 
-def fine_tune_model(model, hyperparams, datasets, history, log_dir, training_tag):
+def fine_tune_model(model_instance, hyperparams, datasets, history, log_dir, training_tag):
     print("Fine tuning")
-    model.base_model.trainable = True  # Setting base model to be trainable again
+    model_instance.base_model.trainable = True  # Setting base model to be trainable again
     fine_tune_from = hyperparams["fine_tune_from"]  # Fine-tune from this layer onwards
-    for layer in model.base_model.layers[:fine_tune_from]:  # Freeze all the layers before the `fine_tune_from` layer
+    for layer in model_instance.base_model.layers[:fine_tune_from]:  # Freeze all the layers before the `fine_tune_from` layer
         layer.trainable = False
 
-    model().compile(
+    model_instance().compile(
         loss=losses.CategoricalCrossentropy(),
         optimizer=optimizers.Adam(lr=hyperparams['base_learning_rate'] / 10),
         metrics=['accuracy']
     )
-    model().summary()
+    model_instance().summary()
 
     total_epochs = hyperparams["initial_epochs"] + hyperparams["fine_tune_epochs"]
-    history_fine_tune = model().fit(
+    history_fine_tune = model_instance().fit(
         x=datasets['train_ds'],
         epochs=total_epochs,
         initial_epoch=history.epoch[-1],
