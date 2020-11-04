6eb6e18f56276d3d95b1e1cfb297e57cbed45786
Jonathan Tan <jontan@palo-it.com>
2020-11-04 12:27:29 +0800

    Updates to log_utils: - Implement plot_class_distribution to plot class distribution for training, validation and test datasets _ Update plot_confusion_matrix to include param for custom plot naming

---
+++ src/log_utils/plot_class_distribution.py
@@ -0,0 +1,43 @@
+from io import BytesIO
+import matplotlib.pyplot as plt
+from typing import List
+import numpy as np
+
+
+def plot_class_distribution(
+        classes: List[str],
+        train_distro: List[int],
+        valid_distro: List[int],
+        test_distro: List[int]):
+    """
+    Returns a file object of matplotlib plot
+
+    Args:
+      classes (array, shape = [str]): An iterable containing strings of class names
+      train_distro (array): Training dataset's distribution
+      valid_distro (array): Validation dataset's distribution
+      test_distro (array): Test dataset's distribution
+    """
+    x_pos = np.array([i for i, _ in enumerate(classes)])
+    bar_width = 0.25
+    list_distro = [
+        ('green', 'train', train_distro),
+        ('blue', 'validation', valid_distro),
+        ('red', 'test', test_distro)
+    ]
+
+    plt.xlabel("Class names")
+    plt.ylabel("Images count")
+    plt.title("Class distribution")
+    plt.xticks(x_pos + 0.25, classes)
+
+    for (color, label, distro) in list_distro:
+        plt.bar(x=x_pos, width=bar_width, height=distro, color=color, label=label)
+        x_pos = x_pos + bar_width
+
+    plt.legend()
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    plt.close()
+    return buffer
--- src/log_utils/plot_confusion_matrix.py
+++ src/log_utils/plot_confusion_matrix.py
@@ -1,19 +1,21 @@
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import Iterable
 
 
-def plot_confusion_matrix(cm, class_names):
+def plot_confusion_matrix(confusion_matrix=None, plot_name="Confusion Matrix", class_names: Iterable[str] = None):
     """
     Returns a matplotlib figure containing the plotted confusion matrix.
 
     Args:
-      cm (array, shape = [n, n]): a confusion matrix of integer classes
+      confusion_matrix (array, shape = [n, n]): a confusion matrix of integer classes
+      plot_name (string): a name for the plot
       class_names (array, shape = [n]): String names of the integer classes
     """
     figure = plt.figure(figsize=(8, 8))
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-    plt.title("Confusion matrix")
+    plt.imshow(confusion_matrix, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title(f"{plot_name.strip()} Confusion")
     plt.colorbar()
     tick_marks = np.arange(len(class_names))
     plt.xticks(tick_marks, class_names, rotation=45)
@@ -23,10 +25,10 @@ def plot_confusion_matrix(cm, class_names):
     # labels = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
 
     # Use white text if squares are dark; otherwise black.
-    threshold = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        color = "white" if cm[i, j] > threshold else "black"
-        plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+    threshold = confusion_matrix.max() / 2.
+    for i, j in itertools.product(range(confusion_matrix.shape[0]), range(confusion_matrix.shape[1])):
+        color = "white" if confusion_matrix[i, j] > threshold else "black"
+        plt.text(j, i, confusion_matrix[i, j], horizontalalignment="center", color=color)
         # plt.text(j, i, labels[i, j], horizontalalignment="center", color=color)
 
     plt.tight_layout()
